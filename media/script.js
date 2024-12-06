011232cfd3bb9d561a5e65ac5d6ac0bb9c1b2213
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-06 19:36:11 +0100

    more

--- media/script.js
+++ media/script.js
@@ -1,6 +1,24 @@
 // Create map
-let map = L.map('map').setView([0, 0], 10);
+let map = L.map('map').setView([0, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: ''
+  attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+let geoLayer;
+let geoData;
+function refreshEarthquakes() {
+  if (geoLayer) map.removeLayer(geoLayer);
+  fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson')
+    .then(res=>res.json())
+    .then(res=>{
+      geoData = res;
+      geoLayer = L.geoJSON(res, {
+        /*onEachFeature: function(feature, layer) {
+          layer.bindPopup(`more data later`);
+        }*/
+      }).addTo(map);
+    })
+}
+
+refreshEarthquakes()
