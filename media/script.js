31d469748b6d73a6fcd45e6a43aaea185656e611
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-06 20:17:31 +0100

    improvements

--- media/script.js
+++ media/script.js
@@ -5,6 +5,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+function timeToColor(time) {
+  let age = (Date.now() - time) / (1000 * 60 * 30);
+  return `hsl(${age}, 75%, 50%)`;
+}
+
 let geoLayer;
 let geoData;
 function refreshEarthquakes() {
@@ -13,12 +18,29 @@ function refreshEarthquakes() {
     .then(res=>res.json())
     .then(res=>{
       geoData = res;
+      document.getElementById('title').innerText = res.metadata.title;
       geoLayer = L.geoJSON(res, {
-        /*onEachFeature: function(feature, layer) {
-          layer.bindPopup(`more data later`);
-        }*/
+        pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, {
+            radius: feature.properties.mag * 2,
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillColor: timeToColor(feature.properties.time),
+            fillOpacity: 0.75
+          });
+        },
+        onEachFeature: function (feature, layer) {
+            // Add popups with earthquake details
+            layer.bindPopup(`
+                <strong>${feature.properties.title}</strong><br>
+                Magnitude: ${feature.properties.mag}<br>
+                Time: ${new Date(feature.properties.time).toLocaleString()}
+            `);
+        }
       }).addTo(map);
     })
 }
 
 refreshEarthquakes()
+setInterval(refreshEarthquakes, 2 * 60 * 1000);
